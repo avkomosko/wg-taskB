edcc0e06b86b103ff8929d4ae4daa87664e2b7fc
Aliaksei <avkomosko@gmail.com>
2021-09-11 22:03:59 +0300

    feat: implement funcion

---
+++ B.js
@@ -0,0 +1,51 @@
+function solve(input) {
+  input = +input;
+  let result = [];
+
+  function progession(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+  function combination(num) {
+    return progession(num) / (progession(num - 2) * progession(2));
+  }
+
+  function countFights(teams, fights) {
+    if (teams === 1) {
+      fights = 0;
+    }
+    if (teams === 2) {
+      fights += 1;
+    }
+    if (teams % 2 !== 0) {
+      fights += combination(teams);
+    }
+
+    if (teams % 2 === 0 && teams !== 2) {
+      fights += teams / 2 + countFights(teams / 2, fights);
+    }
+    return fights;
+  }
+  for (let i = 1; i < input * input; i++) {
+    if (countFights(i, 0) === input) {
+      result.push(i);
+    }
+  }
+
+  if (result[0] === 0) {
+    result[0] = -1;
+  }
+  if (result.length > 1) {
+    result = result.join(' \n ');
+  }
+
+  return result;
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8');
+console.log(solve(input));
